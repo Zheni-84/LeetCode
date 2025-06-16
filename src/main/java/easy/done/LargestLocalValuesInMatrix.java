c873978a30ede4664404a8bc146a4859a8c32e24
Zheni Avramova <z.avramova@tinqin.com>
2025-06-16 22:28:07 +0300

    [easy] Problem 2373 - Largest Local Values in a Matrix: refactor

--- src/main/java/easy/done/LargestLocalValuesInMatrix.java
+++ src/main/java/easy/done/LargestLocalValuesInMatrix.java
@@ -1,4 +1,4 @@
-package easy;
+package easy.done;
 
 /**
  * LeetCode 2373 - Largest Local Values in a Matrix
@@ -13,11 +13,11 @@ public class LargestLocalValuesInMatrix {
 
 	public static void main(String[] args) {
 		int[][] grid = {
-			{1,  2,  3,  4,  5},
-			{5,  6,  7,  8,  9},
-			{9,  10, 11, 12, 13},
-			{13, 14, 15, 16, 17},
-			{17, 18, 19, 20, 21}
+				{ 1, 2, 3, 4, 5 },
+				{ 5, 6, 7, 8, 9 },
+				{ 9, 10, 11, 12, 13 },
+				{ 13, 14, 15, 16, 17 },
+				{ 17, 18, 19, 20, 21 }
 		};
 
 		int[][] result = largestLocalBrute(grid);
@@ -41,17 +41,21 @@ public class LargestLocalValuesInMatrix {
 	private static int[][] largestLocalBrute(int[][] grid) {
 		int n = grid.length;
 		int[][] result = new int[n - 2][n - 2];
+		// Iterate through each 3x3 sub-grid
 		for (int i = 0; i < n - 2; i++) {
 			for (int j = 0; j < n - 2; j++) {
 				int max = Integer.MIN_VALUE;
+				// Find the maximum value in the current 3x3 sub-grid
 				for (int r = i; r <= i + 2; r++) {
 					for (int c = j; c <= j + 2; c++) {
 						max = Math.max(max, grid[r][c]);
 					}
 				}
+				// Store the maximum value in the result grid
 				result[i][j] = max;
 			}
 		}
+
 		return result;
 	}
 }
