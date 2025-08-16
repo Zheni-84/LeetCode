a23b6c8cfe7117697e772008c95a21785e9ca774
Zheni Avramova <z.avramova@tinqin.com>
2025-08-16 23:26:22 +0300

    [easy] Problem 222: Count Complete Tree Nodes: add initial implementation

--- src/main/java/easy/New.java
+++ src/main/java/easy/New.java
@@ -1,5 +1,19 @@
 package easy;
 
+/**
+ * LeetCode Problem: 1436. Destination City
+ * Space: O(n)
+ * Time: O(n)
+ * Topics: Hash Table, String
+ * You are given the array paths, where paths[i] = [cityAi, cityBi] means there exists a direct path going from cityAi to cityBi.
+ * Return the destination city, that is, the city without any path outgoing to another city.
+ * <p>
+ * Example:
+ * Input: paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
+ * Output: "Sao Paulo"
+ * Hint: Use a set to track all starting cities and then find the one that is not in the set.
+ * http://leetcode.com/problems/destination-city/
+ */
 public class New {
 
 }
