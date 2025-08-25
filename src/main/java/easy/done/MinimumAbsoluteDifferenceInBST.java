87e28888dd509564983cab16afe318002c794090
Zheni Avramova <z.avramova@tinqin.com>
2025-08-25 23:15:42 +0300

    [easy] Problem 530: Minimum Absolute Difference in BST: implement minimum difference calculation using in-order traversal

--- src/main/java/easy/done/MinimumAbsoluteDifferenceInBST.java
+++ src/main/java/easy/done/MinimumAbsoluteDifferenceInBST.java
@@ -8,7 +8,7 @@ import java.util.Deque;
  * Time Complexity: O(n), where n is the number of nodes in the tree.
  * Space Complexity: O(h), where h is the height of the tree (for the stack).
  * Topic: Binary Search Tree (BST), In-Order Traversal
- *
+ * <p>
  * This class provides a method to find the minimum absolute difference between values of any two nodes in a BST.
  * The main method demonstrates the usage of the getMinimumDifference method.
  * The TreeNode class is a simple representation of a binary tree node.
@@ -16,6 +16,8 @@ import java.util.Deque;
  * https://leetcode.com/problems/minimum-absolute-difference-in-bst/
  */
 public class MinimumAbsoluteDifferenceInBST {
+	static int min = Integer.MAX_VALUE;
+	static Integer prev = null; // Use Integer to allow null value
 
 	public static void main(String[] args) {
 		TreeNode root = new TreeNode(5);
@@ -29,22 +31,42 @@ public class MinimumAbsoluteDifferenceInBST {
 		// Example: For the BST [0, 2, 4, 5, 8], the minimum absolute difference is 2 (between 2 and 4).
 	}
 
+	private static int minDiffInBST(TreeNode root) {
+		inorder(root);
+		return min;
+	}
+
+	private  static void inorder(TreeNode root) {
+		if (root == null) {
+			return;
+		}
+
+		inorder(root.left);
+		if (prev != null) {
+			min = Math.min(min, Math.abs(root.val - prev));
+		}
+
+		prev = root.val;
+
+		inorder(root.right);
+	}
+
 	private static int getMinimumDifference(TreeNode root) {
 		Deque<TreeNode> stack = new ArrayDeque<>();
 		TreeNode curr = root;
 		Integer prev = null;
 		int minDiff = Integer.MAX_VALUE;
 
-		while(curr != null || !stack.isEmpty()){
+		while (curr != null || !stack.isEmpty()) {
 			//Go left as deep as possible
-			while(curr != null){
+			while (curr != null) {
 				stack.push(curr);
 				curr = curr.left;
 			}
 
 			//Process current node
 			curr = stack.pop();
-			if(prev != null){
+			if (prev != null) {
 				minDiff = Math.min(minDiff, curr.val - prev);
 			}
 			prev = curr.val;
@@ -62,17 +84,8 @@ public class MinimumAbsoluteDifferenceInBST {
 		TreeNode left;
 		TreeNode right;
 
-		TreeNode() {
-		}
-
 		TreeNode(int val) {
 			this.val = val;
-		}
-
-		TreeNode(int val, TreeNode left, TreeNode right) {
-			this.val = val;
-			this.left = left;
-			this.right = right;
 		}
 	}
 }
