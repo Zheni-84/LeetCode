438da50633babef6c2c12909ef9fe369e129febd
Zheni Avramova <z.avramova@tinqin.com>
2025-12-30 00:40:00 +0200

    style: format code for better readability in FindSmallestLetterGreaterThanTarget

--- src/main/java/easy/done/FindSmallestLetterGreaterThanTarget.java
+++ src/main/java/easy/done/FindSmallestLetterGreaterThanTarget.java
@@ -14,18 +14,19 @@ package easy.done;
 public class FindSmallestLetterGreaterThanTarget {
 
 	public static void main(String[] args) {
-		char[] letters = {'c', 'f', 'j'};
+		char[] letters = { 'c', 'f', 'j' };
 		char target = 'a';
 		char result = nextGreatestLetter(letters, target);
 		System.out.println(result); // Output: 'c'
 
-		letters = new char[]{'a', 'z'};
+		letters = new char[] { 'a', 'z' };
 		target = 'm';
 		result = nextGreatestLetter(letters, target);
 		System.out.println(result); // Output: 'z'
 
 		//letters from a to z
-		letters = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+		letters = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+				'w', 'x', 'y', 'z' };
 		target = 'z';
 		result = nextGreatestLetter(letters, target);
 		System.out.println(result); // Output: 'a', no greate than z
@@ -33,13 +34,16 @@ public class FindSmallestLetterGreaterThanTarget {
 
 	private static char nextGreatestLetter(char[] letters, char target) {
 		int len = letters.length - 1;
-		if(letters[0] > target || letters[len] <= target) return letters[0];
+		if (letters[0] > target || letters[len] <= target)
+			return letters[0];
 		int l = 0;
 		int r = len;
-		while(r - l > 1){
-			int mid = (r+l)/2;
-			if(letters[mid] <= target) l = mid;
-			else r = mid;
+		while (r - l > 1) {
+			int mid = (r + l) / 2;
+			if (letters[mid] <= target)
+				l = mid;
+			else
+				r = mid;
 		}
 
 		return letters[r % letters.length];
